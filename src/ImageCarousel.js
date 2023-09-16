b1978ce75f9c49a632e3d86759f38357a5a13a13
Elliot Akande <elbo.lynch@gmail.com>
2023-09-16 17:49:05 +0100

    Add DOM element creation to ImageCarousel

---
+++ src/ImageCarousel.js
@@ -0,0 +1,86 @@
+import svg from "svg";
+import chevronLeft from "./chevron-left.svg";
+import chevronRight from "./chevron-right.svg";
+
+const ImageCarousel = function imageCarouselFactory(
+  images = [],
+  customClass = ""
+) {
+  const createImage = function createImageDOMElement(
+    src,
+    index,
+    indicatorContainer
+  ) {
+    // Container
+    const container = document.createElement("div");
+    container.classList.add("crsl__item");
+
+    // Image
+    const img = document.createElement("img");
+    img.src = src;
+    container.appendChild(img);
+
+    // Add Indicator
+    const indicator = document.createElement("div");
+    indicator.classList.add("crsl__indicator");
+    indicator.dataset.index = index;
+    indicatorContainer.appendChild(indicator);
+
+    return container;
+  };
+
+  const render = function renderImageCarousel() {
+    // Container
+    const carousel = document.createElement("div");
+    carousel.classList.add("crsl");
+
+    // Apply custom CSS class if provided
+    if (customClass !== "") {
+      carousel.classList.add(customClass);
+    }
+
+    // Indicators Container
+    const indicators = document.createElement("div");
+    indicators.classList.add("crsl__indicators");
+    carousel.appendChild(indicators);
+
+    // Images Container
+    const imagesContainer = document.createElement("div");
+    imagesContainer.classList.add("crsl__images");
+    carousel.appendChild(imagesContainer);
+
+    // Images
+    images.forEach((image, index) =>
+      imagesContainer.appendChild(createImage(image, index, indicators))
+    );
+
+    // Set Initial Image
+    indicators
+      .querySelector(".crsl__indicator")
+      .classList.add(".crsl__indicator--active");
+
+    // Previous Control
+    const prevControl = document.createElement("div");
+    prevControl.classList.add("crsl__control", "crsl__control--prev");
+    carousel.appendChild(prevControl);
+
+    const prevIcon = svg(chevronLeft);
+    prevIcon.classList.add("crsl__icon", "crsl__icon--prev");
+    prevControl.appendChild(prevIcon);
+
+    // Next Control
+    const nextControl = document.createElement("div");
+    nextControl.classList.add("crsl__control", "crsl__control--next");
+    carousel.appendChild(nextControl);
+
+    const nextIcon = svg(chevronRight);
+    nextIcon.classList.add("crsl__icon", "crsl__icon--next");
+    nextControl.appendChild(nextIcon);
+
+    return carousel;
+  };
+
+  return render();
+};
+
+export default ImageCarousel;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import ImageCarousel from "./ImageCarousel";
+import img1 from "./img1.jpg";
+import img2 from "./img2.jpg";
+import img3 from "./img3.jpg";
+
+const carousel = ImageCarousel([img1, img2, img3]);
+
+const container = document.querySelector(".container");
+console.log(container);
+container.appendChild(carousel);
