8e266d0cabad5cda3a204459a02ccc26b5315c0e
Elliot Akande <elbo.lynch@gmail.com>
2023-09-17 13:40:25 +0100

    Prepare for bundling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import ImageCarousel from "./ImageCarousel";
-import img1 from "./img1.jpg";
-import img2 from "./img2.jpg";
-import img3 from "./img3.jpg";
-import img4 from "./img4.jpg";
 
-const carousel = ImageCarousel([img1, img2, img3, img4]);
-
-const container = document.querySelector(".container");
-container.appendChild(carousel);
+export default ImageCarousel;
