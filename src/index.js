7e83c6e975d6cd471a7d71aba665f9214b13fc52
Elliot Akande <elbo.lynch@gmail.com>
2023-09-17 09:26:54 +0100

    Add another image

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import ImageCarousel from "./ImageCarousel";
 import img1 from "./img1.jpg";
 import img2 from "./img2.jpg";
 import img3 from "./img3.jpg";
+import img4 from "./img4.jpg";
 
-const carousel = ImageCarousel([img1, img2, img3]);
+const carousel = ImageCarousel([img1, img2, img3, img4]);
 
 const container = document.querySelector(".container");
-console.log(container);
 container.appendChild(carousel);
